1c5bf65e4838d6d3afc1580213514533b845f0f0
Samuel Tang <samuel.x.tang@gmail.com>
2020-09-07 22:35:47 +0800

    ch3.5 - A Recursive approach to ruler drawing

---
+++ ch3/ch3.5_recursion/aRecursiveApproachToRulerDrawing.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void drawOneTick(int tickLength, int tickLabel = -1) {
+  for (int i = 0; i < tickLength; i ++) {
+    cout << '-';
+  }
+
+  if (tickLabel >= 0) {
+    cout << " " << tickLabel;
+  }
+  cout << endl;
+}
+
+void drawTicks(int tickLength) {
+  if (tickLength > 0) {
+    drawTicks(tickLength -1);
+    drawOneTick(tickLength);
+    drawTicks(tickLength -1);
+  }
+}
+
+void drawRuler(int nInches, int majorLength) {
+  drawOneTick(majorLength, 0);
+  for (int i = 1; i <= nInches; i++) {
+    drawTicks(majorLength -1);
+    drawOneTick(majorLength, i);
+  }
+}
+
+int main() {
+  drawRuler(2, 4);
+  cout << endl << endl;
+  drawRuler(1, 5);
+  cout << endl << endl;
+  drawRuler(3, 3);
+  cout << endl << endl;
+  drawRuler(10, 5);
+  cout << endl << endl;
+}
---
+++ ch3/ch3.5_recursion/test-aRecursiveApproachToRulerDrawing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+g++ aRecursiveApproachToRulerDrawing.cpp -o aRecursiveApproachToRulerDrawing
+./aRecursiveApproachToRulerDrawing
+rm aRecursiveApproachToRulerDrawing
